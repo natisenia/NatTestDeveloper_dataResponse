d168a6d427ce49943b92de8105415bf23b404a1e
Nat Isenia <nat.isenia88@gmail.com>
2022-04-07 19:16:34 +0100

    Data fetched and loading text  is implemented.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
+import React, { useState, useEffect } from 'react';
 import logo from './simpplr.svg';
 import styles from './App.module.css';
 
-import { getBlocks } from './blocks';
+import { Block, getBlocks } from './blocks';
 
 import { InfoPanel } from './InfoPanel/InfoPanel';
 import { ImageGrid } from './ImageGrid/ImageGrid';
 
 export const App = () => {
+
+  const [data, setData] = useState<Block>();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    getBlocks()
+      .then((newData) => {
+        setData(newData);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setData(undefined);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+console.log('data', data);
+
   return (
     <div>
       <header className={styles.header}>
@@ -16,6 +38,8 @@ export const App = () => {
         <ImageGrid />
         <InfoPanel />
       </main>
+      {loading &&
+      <p>Blocks are loading. Please wait...</p>}
     </div>
   );
 };
